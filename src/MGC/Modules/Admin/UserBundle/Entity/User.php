77a9b0d6481de3dbd9333ec1d33536a7d576e4d3
Alexandre <alexandre.dupre.web@free.fr>
2016-08-05 11:36:26 +0200

    feat: create user entity
    Add User MGC Entity

---
+++ src/MGC/Modules/Admin/UserBundle/Entity/User.php
@@ -0,0 +1,469 @@
+<?php
+
+namespace MGC\Modules\Admin\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="MGC\Modules\Admin\UserBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255, unique=true)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="gender", type="string", length=10)
+     */
+    private $gender;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sname", type="string", length=255)
+     */
+    private $sname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fname", type="string", length=255)
+     */
+    private $fname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="avatar", type="string", length=255)
+     */
+    private $avatar;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_company", type="integer", nullable=true)
+     */
+    private $idCompany;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="company_job", type="string", length=255, nullable=true)
+     */
+    private $companyJob;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_birth", type="date", nullable=true)
+     */
+    private $dateBirth;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_created", type="datetime")
+     */
+    private $dateCreated;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_updated", type="datetime")
+     */
+    private $dateUpdated;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="active", type="boolean", unique=true)
+     */
+    private $active;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ordre", type="integer", nullable=true)
+     */
+    private $ordre;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     *
+     * @return User
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param string $gender
+     *
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set sname
+     *
+     * @param string $sname
+     *
+     * @return User
+     */
+    public function setSname($sname)
+    {
+        $this->sname = $sname;
+
+        return $this;
+    }
+
+    /**
+     * Get sname
+     *
+     * @return string
+     */
+    public function getSname()
+    {
+        return $this->sname;
+    }
+
+    /**
+     * Set fname
+     *
+     * @param string $fname
+     *
+     * @return User
+     */
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+
+        return $this;
+    }
+
+    /**
+     * Get fname
+     *
+     * @return string
+     */
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     *
+     * @return User
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set idCompany
+     *
+     * @param integer $idCompany
+     *
+     * @return User
+     */
+    public function setIdCompany($idCompany)
+    {
+        $this->idCompany = $idCompany;
+
+        return $this;
+    }
+
+    /**
+     * Get idCompany
+     *
+     * @return int
+     */
+    public function getIdCompany()
+    {
+        return $this->idCompany;
+    }
+
+    /**
+     * Set companyJob
+     *
+     * @param string $companyJob
+     *
+     * @return User
+     */
+    public function setCompanyJob($companyJob)
+    {
+        $this->companyJob = $companyJob;
+
+        return $this;
+    }
+
+    /**
+     * Get companyJob
+     *
+     * @return string
+     */
+    public function getCompanyJob()
+    {
+        return $this->companyJob;
+    }
+
+    /**
+     * Set dateBirth
+     *
+     * @param \DateTime $dateBirth
+     *
+     * @return User
+     */
+    public function setDateBirth($dateBirth)
+    {
+        $this->dateBirth = $dateBirth;
+
+        return $this;
+    }
+
+    /**
+     * Get dateBirth
+     *
+     * @return \DateTime
+     */
+    public function getDateBirth()
+    {
+        return $this->dateBirth;
+    }
+
+    /**
+     * Set dateCreated
+     *
+     * @param \DateTime $dateCreated
+     *
+     * @return User
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return \DateTime
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Set dateUpdated
+     *
+     * @param \DateTime $dateUpdated
+     *
+     * @return User
+     */
+    public function setDateUpdated($dateUpdated)
+    {
+        $this->dateUpdated = $dateUpdated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateUpdated
+     *
+     * @return \DateTime
+     */
+    public function getDateUpdated()
+    {
+        return $this->dateUpdated;
+    }
+
+    /**
+     * Set active
+     *
+     * @param boolean $active
+     *
+     * @return User
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return bool
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+
+    /**
+     * Set ordre
+     *
+     * @param integer $ordre
+     *
+     * @return User
+     */
+    public function setOrdre($ordre)
+    {
+        $this->ordre = $ordre;
+
+        return $this;
+    }
+
+    /**
+     * Get ordre
+     *
+     * @return int
+     */
+    public function getOrdre()
+    {
+        return $this->ordre;
+    }
+}
+
