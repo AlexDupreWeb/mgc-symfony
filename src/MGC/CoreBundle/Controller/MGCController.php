16872ddda75d02e5c2720f458f14c011043e20c5
Alexandre Dupré <alexandre.dupre.web@free.fr>
2016-10-28 09:12:59 +0200

    Update Home (#1)
    feat: update home
    
    add adminlte config file
    add icons config file
    add HomeWidget class
    add translation files

--- src/MGC/CoreBundle/Controller/MGCController.php
+++ src/MGC/CoreBundle/Controller/MGCController.php
@@ -9,4 +9,23 @@ class MGCController extends Controller
 {
     public function __construct() {}
 
+    public function getIcon($name, $theme='default'){
+        $icons = $this->container->getParameter('icons');
+
+        if(isset($icons[$theme][$name])){
+            return $icons[$theme][$name];
+        }elseif(isset($icons[$theme]['no_icon'])){
+            return $icons[$theme]['no_icon'];
+        }
+
+        return null;
+    }
+
+    public function getAdminlteParams(){
+        $params = $this->container->getParameter('adminlte');
+
+        dump($params);
+        die();
+    }
+
 }
