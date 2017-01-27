79f6ed6d17c5e8ee70d16442a1f843c989ac523b
Alexandre <alexandre.dupre.web@free.fr>
2017-01-27 21:26:35 +0100

    feat: add command to create Elements for demo
    And create accounts.yml to contains data

---
+++ src/JSP/AdminBundle/Command/InsertDataForDemoCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace JSP\AdminBundle\Command;
+
+use JSP\AdminBundle\Services\Mappers\AccountMapper;
+use JSP\AdminBundle\Services\Mappers\ElementMapper;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+
+class InsertDataForDemoCommand extends ContainerAwareCommand {
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure() {
+        $this
+            ->setName('jsp:data:demo')
+            ->setDescription('Insert data for demo');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        // CALL TO CONTAINER SERVICES
+        $kernel = $this->getContainer()->get('kernel');
+        $encoder = $this->getContainer()->get('security.password_encoder');
+        $em = $this->getContainer()->get('doctrine')->getEntityManager('default');
+
+        $this->addAccounts($input, $output, $kernel, $em);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param $kernel
+     * @param $em
+     */
+    private function addAccounts(InputInterface $input, OutputInterface $output, $kernel, $em) {
+        $accountMapper = new AccountMapper();
+
+        $output->writeln('<info>[BEGIN] Prepare Accounts</info>');
+        $output->writeln('');
+
+        // YAML
+        $yamlPath = $kernel->locateResource('@JspAdminBundle/Resources/config/accounts.yml');
+        $yamlContent = Yaml::parse(file_get_contents($yamlPath));
+        $yamlAccounts = $yamlContent['accounts']['demo'];
+
+        // ACCOUNTS
+        $accounts = array();
+        foreach($yamlAccounts as $yamlAccount) {
+            $accounts[] = $accountMapper->yamlToEntity($yamlAccount);
+        }
+
+        $output->writeln('<info> - Insert Accounts</info>');
+        $output->writeln('');
+
+        foreach($accounts as $account) {
+            $em->persist($account);
+            $em->flush();
+        }
+
+        $output->writeln('<info>[END] Accounts created!</info>');
+    }
+
+}
