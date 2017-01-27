cf2de823388b30bef48f5d527d46cc7b28f68434
Alexandre <alexandre.dupre.web@free.fr>
2017-01-27 21:37:16 +0100

    feat: call services.yml in JSP AdminBundle

--- src/JSP/AdminBundle/JspAdminBundle.php
+++ src/JSP/AdminBundle/JspAdminBundle.php
@@ -2,8 +2,13 @@
 
 namespace JSP\AdminBundle;
 
+use JSP\AdminBundle\DependencyInjection\JspAdminServiceExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JspAdminBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new JspAdminServiceExtension();
+    }
 }
