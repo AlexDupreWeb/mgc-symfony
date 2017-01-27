916b6b4c112318b6126bed22c5f22286df0251dd
Alexandre <alexandre.dupre.web@free.fr>
2017-01-27 21:23:15 +0100

    feat: add AdminController to JSP
    Create default view index.html.twig

--- src/JSP/AdminBundle/Controller/AdminController.php
+++ src/JSP/AdminBundle/Controller/AdminController.php
@@ -2,16 +2,18 @@
 
 namespace JSP\AdminBundle\Controller;
 
+use MGC\CoreBundle\Controller\MGCController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class AdminController extends Controller
-{
+class AdminController extends MGCController {
+
     /**
-     * @Route("/jsp/")
+     * @Route("/jsp/", name="jsp_home")
      */
     public function indexAction()
     {
-        return $this->render('JspAdminBundle:Default:index.html.twig');
+        return $this->render('JspAdminBundle:Admin:index.html.twig');
     }
+
 }
