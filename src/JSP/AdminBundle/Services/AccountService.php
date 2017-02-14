d0c0a15c7acf6da7892fa6d54b4806264fadbf66
Alexandre <alexandre.dupre.web@free.fr>
2017-02-14 23:20:46 +0100

    feat: add AccountService

---
+++ src/JSP/AdminBundle/Services/AccountService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace JSP\AdminBundle\Services;
+
+use Doctrine\ORM\EntityManager;
+use JSP\CoreBundle\Entity\Account;
+use JSP\CoreBundle\Repository\AccountRepository;
+
+class AccountService {
+
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @var AccountRepository
+     */
+    private $accountRepository;
+
+    /**
+     * AccountService constructor.
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em) {
+        $this->em = $em;
+
+        $this->accountRepository = $em->getRepository('JspCoreBundle:Account');
+    }
+
+    public function createAccount(Account $account) {
+        $now = new \DateTime();
+
+        $account
+            ->setDateCreated($now)
+            ->setDateUpdated($now)
+        ;
+
+        $this->em->persist($account);
+        $this->em->flush();
+
+        return $account;
+    }
+
+    public function updateAccount(Account $account) {
+        $now = new \DateTime();
+
+        $account->setDateUpdated($now);
+
+        $this->em->persist($account);
+        $this->em->flush();
+    }
+
+    public function deleteAccount() {
+
+    }
+}
