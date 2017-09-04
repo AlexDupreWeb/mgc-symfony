9c14df3234a26932e9b398504766f2fdf5307979
Alexandre <alexandre.dupre.web@free.fr>
2017-09-04 22:05:23 +0200

    feat: update default Controller and default view

--- src/MGC/TimeTrackingBundle/Controller/DefaultController.php
+++ src/MGC/TimeTrackingBundle/Controller/DefaultController.php
@@ -6,13 +6,47 @@ use MGC\CoreBundle\Controller\MGCController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends MGCController
-{
+class DefaultController extends MGCController {
+
     /**
      * @Route("/time-tracking")
      */
-    public function indexAction()
-    {
-        return $this->render('TimeTrackingBundle:Default:index.html.twig');
+    public function indexAction() {
+
+        $projects = $this->getDoctrine()->getRepository('TimeTrackingBundle:Project')->findAll();
+        $tasks = $this->getDoctrine()->getRepository('TimeTrackingBundle:Task')
+            ->findBy([], ['date' => 'desc']);
+
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $query = $em->createQueryBuilder();
+
+        $query
+            ->select('t.date')
+            ->addSelect('SUM(t.executionTime) executionTime')
+            ->from('TimeTrackingBundle:Task', 't')
+            ->groupBy('t.date')
+            ->orderBy('t.date', 'DESC')
+            //->setMaxResults( $limit )
+        ;
+        $daily_report = $query->getQuery()->getArrayResult();
+
+        for ($i = 0; $i < count($daily_report); $i++) {
+            if($daily_report[$i]['executionTime'] >= (7*60)) {
+                $daily_report[$i]['color'] = 'green';
+            }elseif($daily_report[$i]['executionTime'] < (7*60) && $daily_report[$i]['executionTime'] >= (4*60)) {
+                $daily_report[$i]['color'] = 'yellow';
+            }else{
+                $daily_report[$i]['color'] = 'red';
+            }
+        }
+
+        return $this->render('TimeTrackingBundle:Default:index.html.twig', [
+            'projects' => $projects,
+            'tasks' => $tasks,
+            'daily_report' => $daily_report,
+        ]);
+
     }
+
 }
